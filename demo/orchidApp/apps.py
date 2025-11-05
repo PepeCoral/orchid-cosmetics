2fed54940736b99c2ba8bf0556c54ee8cc59d70b
Miguel Garcia Vizcaino <miguelgvizcaino@gmail.com>
2025-11-05 20:13:10 +0100

    fix: :fire: mover al nuevo sistema de archivos

--- demo/orchidApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class OrchidappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'orchidApp'
--- src/app/models.py
+++ src/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=40)
+    surname = models.CharField(max_length=100, blank=True)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    address = models.CharField(max_length=200, blank=True, default='')
+    payMethod = models.CharField(max_length=50, blank=True, default='')
+    role = models.CharField(max_length=20, blank=True, default='')
+
