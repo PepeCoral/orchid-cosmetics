34126810fa1e44865a13ec3b3b20f7d0eb153bb4
Emilio-115 <emivazcru@alum.us.es>
2025-11-06 19:56:40 +0100

    feature :sparkles: User model now inherits AbstractUser, duplicate fields were removed

--- src/app/models/user.py
+++ src/app/models/user.py
@@ -1,13 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    surname = models.CharField(max_length=100, blank=True)
-    email = models.EmailField()
-    password = models.CharField(max_length=100)
+
+class RoleOptions(models.TextChoices):
+    USER = 'User'
+    ADMIN = 'Admin'
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100, blank=True)
     address = models.CharField(max_length=200, blank=True, default='')
-    payMethod = models.CharField(max_length=50, blank=True, default='')
-    role = models.CharField(max_length=20, blank=True, default='')
+    pay_method = models.CharField(max_length=50, blank=True, default='')
+    role = models.CharField(max_length=20, choices=RoleOptions.choices, default=RoleOptions.USER)
     
     def __str__(self):
-        return f"{self.name} {self.surname} - {self.email}"
+        return f"{self.first_name} {self.last_name} - {self.email}"
