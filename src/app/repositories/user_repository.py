8a5277544e99aecb9f2a685b6370d47e81773f28
Miguel Garcia Vizcaino <miguelgvizcaino@gmail.com>
2025-11-07 12:55:09 +0100

    feat: :art: cambiar nombres

---
+++ src/app/repositories/user_repository.py
@@ -0,0 +1,20 @@
+from app.repositories.base_repository import BaseRepository
+from app.models.user import User
+
+class UserRepository(BaseRepository):
+    def __init__(self):
+        super().__init__(User)
+
+    # Ejemplo de métodos personalizados
+    def get_by_email(self, email):
+        return self.model.objects.filter(email=email)
+
+    def is_admin(self, role):
+        return role == User.RoleOptions.ADMIN
+    
+    def get_address(self):
+        return self.model.objects.values_list('address', flat=True).distinct()
+    
+
+    
+
