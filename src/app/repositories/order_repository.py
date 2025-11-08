57019fe6f751df509172bb3d1d7682d58a90607d
Miguel Garcia Vizcaino <miguelgvizcaino@gmail.com>
2025-11-08 12:51:35 +0100

    feat: :zap: añadidos repositorios y pruebas de product y service quantity

--- src/app/repositories/order_repository.py
+++ src/app/repositories/order_repository.py
@@ -1,18 +1,22 @@
 from app.repositories.base_repository import BaseRepository
 from app.models.order import Order
+from app.repositories.product_repository import ProductRepository
 
 class OrderRepository(BaseRepository):
     def __init__(self):
         super().__init__(Order)
+        self.product_repository = ProductRepository()
 
     def get_orders_by_user(self, user_id):
         return self.model.objects.filter(user_id=user_id)
-
-    def get_order_by_id(self, order_id):
-        return self.model.objects.filter(id=order_id).first()
     
     def get_orders_by_status(self, status):
         return self.model.objects.filter(status=status)
     
     def get_orders_by_identifier(self, identifier):
         return self.model.objects.filter(identifier=identifier)
+    
+    def get_number_of_products(self, product_id, order_id):
+        product = self.product_repository.get_by_id(product_id)
+        order = self.get_by_id(order_id)
+        
