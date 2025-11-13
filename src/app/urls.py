0ac469e1ec20f362329aacf3af7b8ac55b129cfb
Miguel Garcia Vizcaino <miguelgvizcaino@gmail.com>
2025-11-13 20:35:15 +0100

    feat: :zap: añadir login, signup y modificar base.html

--- src/app/urls.py
+++ src/app/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('profile/', views.profile, name='profile'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
 
 ]
--- src/app/views.py
+++ src/app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse
 from .models import User
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 def home(request):
     return render(request, "home.html")
@@ -9,3 +11,28 @@ def home(request):
 def profile(request):
     items = User.objects.all()
     return render(request, "profile.html", {'users': items})
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        
+        # Autenticar usuario
+        user = authenticate(request, username=username, password=password)
+
+        print(user)
+        if user is not None:
+            # Iniciar sesión
+            login(request, user)
+            messages.success(request, f"¡Bienvenido, {user.username}!")
+            return redirect("home")  # Redirige a la página principal
+        else:
+            # Error de credenciales
+            messages.error(request, "Usuario o contraseña incorrectos")
+        
+    # Si es GET o falló el login, renderiza el formulario
+    return render(request, "login.html")
+
+
+def signup(request):
+    return render(request, "signup.html")
