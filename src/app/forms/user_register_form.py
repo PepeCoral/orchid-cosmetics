23f22604218d427b92b080332a95e33f9045c6c6
Emilio-115 <emivazcru@alum.us.es>
2025-11-14 16:47:45 +0100

    feat: :sparkles: Create form for register users

---
+++ src/app/forms/user_register_form.py
@@ -0,0 +1,29 @@
+from django.forms import ModelForm
+from django import forms
+from app.models import User
+from django.core.validators import MaxLengthValidator,EmailValidator, MinLengthValidator
+
+
+
+class UserRegisterForm(forms.Form):
+    username = forms.CharField(label="Nombre de usuario",validators=[MaxLengthValidator(150,"Nombre de usuario demasiado largo")])
+    first_name = forms.CharField(label="Nombre/s", validators=[MaxLengthValidator(100,"First name too long")])
+    last_name = forms.CharField(label="Apellidos", validators=[MaxLengthValidator(100,"Last name too long")])
+    email = forms.EmailField(label="Email", validators=[EmailValidator("Formato incorrecto")])
+    address = forms.CharField(label="Dirección",validators=[MaxLengthValidator(200,"Address too long")],required=False)
+    pay_method = forms.CharField(label="Método de pago", required=False)
+    password = forms.CharField(label="Introduzca una contraseña", 
+                               validators=[MinLengthValidator(6,"Contraseña demasiado corta")], 
+                               widget=forms.PasswordInput())
+    confirm_password = forms.CharField(label="Escriba de nuevo la contraseña", 
+                                       validators=[MinLengthValidator(6,"Contraseña demasiado corta")], 
+                                       widget=forms.PasswordInput())
+
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password and confirm_password and password != confirm_password:
+            self.add_error("confirm_password", "Las contraseñas no coinciden")
