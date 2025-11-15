0f67a6a6688f134d9e229dd92c053c073b2ed398
Emilio-115 <emivazcru@alum.us.es>
2025-11-15 12:45:36 +0100

    fix: :bug: Now negative stock is not possible

--- src/app/models/product.py
+++ src/app/models/product.py
@@ -1,12 +1,13 @@
 from django.db import models
 from .category import Category
 from .order import Order
+from django.core.validators import MinValueValidator
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.IntegerField()
+    stock = models.IntegerField(validators=[MinValueValidator(0, "No puede haber stock negativo")])
     fabricator = models.CharField(max_length=100)
     image_url = models.URLField(blank=True)
     categories = models.ManyToManyField(Category)
