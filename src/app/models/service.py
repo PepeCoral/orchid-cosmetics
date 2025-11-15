ac84a59790f088dafe8dfafa43a3d69cdfa4618d
Emilio-115 <emivazcru@alum.us.es>
2025-11-15 13:11:49 +0100

    feat: :sparkles: Add min value restrictions

--- src/app/models/service.py
+++ src/app/models/service.py
@@ -1,12 +1,13 @@
 from django.db import models 
 from .category import Category
 from .order import Order
+from django.core.validators import MinValueValidator
 
 class Service(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    duration_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.0)])
+    duration_minutes = models.IntegerField(validators=[MinValueValidator(0)])
     department = models.CharField(max_length=100)
     image_url = models.URLField(null=True)
     categories = models.ManyToManyField(Category)
