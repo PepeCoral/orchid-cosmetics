1999589857b64b90f1a44f1a7e2a616f5f34cfdf
Emilio-115 <emivazcru@alum.us.es>
2025-11-15 16:58:22 +0100

    refactor: :fire: Delete unused code

--- src/app/views/__init__.py
+++ src/app/views/__init__.py
@@ -6,9 +6,5 @@ from .user_controller import (
     check_auth
 )
 from .service_controller import (
-    create_service, get_service, list_services, get_services_by_category,
-    get_services_by_department, update_service, delete_service, search_services,
-    get_services_by_price_range, get_services_by_duration, get_popular_services,
-    get_services_sorted_by_price, get_services_sorted_by_duration,
-    service_categories_overview
+    create_service, get_service, list_services
 )
