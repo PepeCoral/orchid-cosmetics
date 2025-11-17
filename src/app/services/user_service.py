c980c9f2065cee0599f4053d0794d2f533f4db6f
Juan Prieto <juaprifer@alum.us.es>
2025-11-17 17:34:18 +0100

    feat: :sparkles: update y delete en user service

--- src/app/services/user_service.py
+++ src/app/services/user_service.py
@@ -1,5 +1,6 @@
-from django.core.exceptions import ValidationError
+from app.models.user import RoleOptions, User
 from app.repositories.user_repository import UserRepository
+from django.core.exceptions import ValidationError, PermissionDenied
 
 class UserService():
     def __init__(self):
@@ -20,3 +21,38 @@ class UserService():
         data.pop("confirm_password", None)
 
         return self.user_repository.create(**data)
+
+    def update_user(self, user_id, user_data, request_user):
+        user_to_update = self.user_repository.get_by_id(user_id)
+        if not user_to_update:
+            raise ValidationError("Usuario no encontrado.")
+
+        if request_user.id != user_to_update.id:
+            raise PermissionDenied("Solo puedes modificar tu propio perfil.")
+
+        if 'username' in user_data:
+            existing_user = self.user_repository.get_by_username(user_data["username"])
+            if existing_user and existing_user.id != user_id:
+                raise ValidationError("Username already in use")
+
+        if 'email' in user_data:
+            existing_user = self.user_repository.get_by_email(user_data["email"])
+            if existing_user and existing_user.id != user_id:
+                raise ValidationError("Email already in use")
+
+        if 'password' in user_data and user_data['password']:
+            user_to_update.set_password(user_data['password'])
+            user_to_update.save()
+            user_data.pop('password', None)
+
+        return self.user_repository.update(user_id, **user_data)
+
+    def delete_user(self, user_id, request_user):
+        user_to_delete = self.user_repository.get_by_id(user_id)
+        if not user_to_delete:
+            raise ValidationError("Usuario no encontrado.")
+
+        if request_user.id != user_to_delete.id:
+            raise PermissionDenied("Solo puedes eliminar tu propio perfil.")
+
+        return self.user_repository.delete(user_id)
