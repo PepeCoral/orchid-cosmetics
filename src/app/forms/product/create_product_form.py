8d0a070439c44601602167262f3199481c6816f7
Pepe Coral <coritochico@gmail.com>
2025-11-17 11:23:12 +0100

    feat: :sparkles: added a validation for negative price and stock

--- src/app/forms/product/create_product_form.py
+++ src/app/forms/product/create_product_form.py
@@ -4,13 +4,36 @@ from django.core.validators import MaxLengthValidator
 
 
 class CreateProductForm(forms.Form):
-    name = forms.CharField(label="Nombre del producto",validators=[MaxLengthValidator(150,"Nombre del producto demasiado largo")])
-    description = forms.CharField(label="Descripcion", validators=[MaxLengthValidator(300,"Description too long")])
-    price = forms.DecimalField(label="Precio", decimal_places=2)
-    stock = forms.IntegerField(label="Stock", required=True)
-    fabricator = forms.CharField(label="Fabricante",validators=[MaxLengthValidator(200,"Fabricator too long")],required=False)
-    image = forms.ImageField(label="Imagen del producto",required=False)
-    categories = forms.ModelMultipleChoiceField(queryset=Category.objects.all(),
-                                      widget=forms.SelectMultiple,
-                                      required=False,
-                                      label="Categorias")
+
+    name = forms.CharField(
+        label="Nombre del producto",
+        validators=[MaxLengthValidator(150,"Nombre del producto demasiado largo")])
+
+    description = forms.CharField(
+        label="Descripcion",
+        validators=[MaxLengthValidator(300,"Description too long")])
+
+    price = forms.DecimalField(
+        label="Precio",
+        decimal_places=2,
+        min_value=0)
+
+    stock = forms.IntegerField(
+        label="Stock",
+        required=True,
+        min_value=0)
+
+    fabricator = forms.CharField(
+        label="Fabricante",
+        validators=[MaxLengthValidator(200,"Fabricator too long")],
+        required=False)
+
+    image = forms.ImageField(
+        label="Imagen del producto",
+        required=False)
+
+    categories = forms.ModelMultipleChoiceField(
+        queryset=Category.objects.all(),
+        widget=forms.SelectMultiple,
+        required=False,
+        label="Categorias")
