8cb5c5e4719e06058cf182fcac36ff7902c8e203
Juan Prieto <juaprifer@alum.us.es>
2025-11-17 18:08:55 +0100

    feat: :sparkles: url para delete y update

--- src/app/urls.py
+++ src/app/urls.py
@@ -3,9 +3,11 @@ import app.views as views
 from django.contrib.auth import views as auth_views
 
 from app.views.home_view import HomeView
+from app.views.user.user_delete_view import DeleteUserView
 from app.views.user.user_register_view import UserRegisterView
 from app.views.user.user_logout_view import UserLogoutView
 from app.views.user.user_profile_view import UserProfileView
+
 
 from app.views.catalog.catalog_view import CatalogView
 
@@ -14,11 +16,13 @@ from app.views.admin.category.list_category_view import ListCategoryView
 from app.views.admin.category.create_category_view import CreateCategoryView
 from app.views.admin.category.delete_category_view import DeleteCategoryView
 
+
 from app.views.admin.product.list_product_view import ListProductView
 from app.views.admin.product.create_product_view import CreateProductView
 from app.views.admin.product.show_product_view import ShowProductView
 from app.views.admin.product.delete_product_view import DeleteProductView
 from app.views.admin.product.update_product_view import UpdateProductView
+from app.views.user.user_update_view import UpdateUserView
 
 urlpatterns = [
     # Páginas HTML (Template views)
@@ -27,7 +31,10 @@ urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="user/login.html", redirect_authenticated_user=True, next_page="/profile" ), name='login'),
     path("logout/", UserLogoutView.as_view(), name='logout'),
     path("register/", UserRegisterView.as_view(), name="register"),
+    
 
+    path('user/update/<int:user_id>/', UpdateUserView.as_view(), name='update_user'),
+    path('user/delete/<int:user_id>/', DeleteUserView.as_view(), name='delete_user'),
 
     path("catalog/", CatalogView.as_view(), name="catalog" ),
 
@@ -37,12 +44,12 @@ urlpatterns = [
     path('admin/categories/create/',CreateCategoryView.as_view(), name='admin/categories/create'),
     path('admin/categories/delete/<int:category_id>',DeleteCategoryView.as_view(), name='admin/categories/delete'),
 
+
     path('admin/products/', ListProductView.as_view(), name='admin/products'),
     path('admin/products/create/', CreateProductView.as_view(), name='admin/products/create'),
     path('admin/products/<int:product_id>/', ShowProductView.as_view(), name='admin/products/show'),
     path('admin/products/delete/<int:product_id>/', DeleteProductView.as_view(), name='admin/products/delete'),
     path('admin/products/update/<int:product_id>/', UpdateProductView.as_view(), name='admin/products/update'),
-
 
     # CRUD básico
     path('services/', views.list_services, name='list_services'),
@@ -52,6 +59,7 @@ urlpatterns = [
     path('cash/', views.get_all_quantity, name="get_all_quantity"),
     path('orders/create/', views.create_order, name="create_order"),
     path('orders/', views.get_all_orders, name="get_all_orders"),
+
     # path('services/<int:service_id>/update/', views.update_service, name='update_service'),
     # path('services/<int:service_id>/delete/', views.delete_service, name='delete_service'),
 
@@ -69,4 +77,5 @@ urlpatterns = [
     # # Otros
     # path('services/popular/', views.get_popular_services, name='popular_services'),
     # path('services/categories-overview/', views.service_categories_overview, name='service_categories_overview'),
+
 ]
