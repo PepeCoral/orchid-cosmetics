2b64d496337e6dc900b7ba2f3af21b38443a94af
Juan Prieto <juaprifer@alum.us.es>
2025-11-18 17:03:28 +0100

    style: :lipstick: catalogo funcional

--- src/app/views/catalog/catalog_view.py
+++ src/app/views/catalog/catalog_view.py
@@ -3,15 +3,22 @@ from django.shortcuts import render, redirect
 from app.services.product_service import ProductService
 
 from app.forms.catalog.search_catalog_form import SearchCatalogForm
+from app.services.service_service import ServiceService
 class CatalogView(View):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.product_service = ProductService()
+        self.service_service = ServiceService()
 
     def get(self, request):
-        form = SearchCatalogForm()
+        # Obtener todos los productos y servicios
         products = self.product_service.get_all_products()
-        return render(request, "catalog.html", {"form": form, "products": products})
+        services = self.service_service.get_all_services()
+        
+        return render(request, "catalog/catalog.html", {
+            "products": products,
+            "services": services
+        })
 
     def post(self, request):
 
