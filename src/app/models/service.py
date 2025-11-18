1bda217fca64d98dff3d571f27ad2b5b1111a213
Emilio-115 <emivazcru@alum.us.es>
2025-11-18 12:14:47 +0100

    fix: :bug: Correct bug when price too elevated

--- src/app/models/service.py
+++ src/app/models/service.py
@@ -1,12 +1,13 @@
 from django.db import models 
 from app.models.category import Category
 from .order import Order
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator,MaxValueValidator
 
 class Service(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.0)])
+    price = models.DecimalField(max_digits=10, decimal_places=2, 
+                                validators=[MinValueValidator(0.0),MaxValueValidator(99999999.99)])
     duration_minutes = models.IntegerField(validators=[MinValueValidator(0)])
     department = models.CharField(max_length=100)
     image_url = models.ImageField(upload_to='services/', null=True)
