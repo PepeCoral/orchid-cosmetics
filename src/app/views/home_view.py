34246824a998e0c7aff7d04fe4c3d172c32e3873
Miguel Garcia Vizcaino <miguelgvizcaino@gmail.com>
2025-11-19 20:32:15 +0100

    feat: :art: añadir escaparate a la pestaña principal

--- src/app/views/home_view.py
+++ src/app/views/home_view.py
@@ -1,10 +1,20 @@
 from django.views import View
 from django.shortcuts import render
-
+from app.services.product_service import ProductService
+from app.services.service_service import ServiceService
 
 class HomeView(View):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.product_service = ProductService()
+        self.service_service = ServiceService()
 
     def get(self, request):
-        return render(request, "home.html")
+        products = self.product_service.get_all_products()
+        services = self.service_service.get_all_services()
+
+        context = {
+            "products": products,   
+            "services": services
+        }
+        return render(request, "home.html", context)
