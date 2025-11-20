20a6544d0c0497d02df2977aa98b582a4959455c
juanprietofernandez <114418131+juanprietofernandez@users.noreply.github.com>
2025-11-20 19:36:40 +0100

    Update update_product_view.py

--- src/app/views/admin/product/update_product_view.py
+++ src/app/views/admin/product/update_product_view.py
@@ -2,7 +2,6 @@ from django.views import View
 from django.shortcuts import render, redirect
 from app.forms.product.update_product_form import UpdateProductForm
 from app.services.product_service import ProductService
-
 class UpdateProductView(View):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -18,10 +17,7 @@ class UpdateProductView(View):
         product = self.product_service.get_product_by_id(product_id)
         form = UpdateProductForm(instance=product)
 
-        return render(request, "admin/products/update.html", {
-            "form": form, 
-            "product": product  # ← Añade el producto al contexto
-        })
+        return render(request, "admin/products/update.html", {"form": form})
 
     def post(self, request, product_id):
         if request.user.is_anonymous:
@@ -30,30 +26,18 @@ class UpdateProductView(View):
         if not request.user.is_superuser:
             return redirect("/")
 
-        # Obtener el producto primero
         product = self.product_service.get_product_by_id(product_id)
-        
-
         form = UpdateProductForm(request.POST, request.FILES, instance=product)
 
         if not form.is_valid():
-            return render(request, "admin/products/update.html", {
-                "form": form, 
-                "product": product,
-                "error": "Por favor, corrige los errores del formulario."
-            })
+            return render(request, "admin/products/update.html", {"form": form})
 
         try:
-
-            form.save()
+            self.product_service.update_product(product_id, form.cleaned_data, request)
             return redirect(f"/admin/products/{product_id}")
         except Exception as e:
             return render(
                 request,
                 "admin/products/update.html",
-                {
-                    "form": form, 
-                    "product": product,
-                    "error": str(e)
-                }
+                {"form": form, "error": str(e)}
             )
