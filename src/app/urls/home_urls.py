b5aad0bcc6c09b59698f2f544177a0ce9d273800
Miguel Garcia Vizcaino <miguelgvizcaino@gmail.com>
2025-11-21 17:58:05 +0100

    feat: :zap: añadir buscador y restricciones para no promover un producto agotado

--- src/app/urls/home_urls.py
+++ src/app/urls/home_urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from app.views.home_view import HomeView
+from app.views.home.home_view import HomeView
 from app.views.static_pages import ContactView
+from app.views.home.search_product_view import SearchProductView
+from app.views.home.search_service_view import SearchServiceView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('contacto/', ContactView.as_view(), name='contact'),
+    path('search/product', SearchProductView.as_view(), name='search/product'),
+    path('search/service',SearchServiceView.as_view(), name='search/service')
 ]
--- src/app/views/home/search_product_view.py
+++ src/app/views/home/search_product_view.py
@@ -6,25 +6,14 @@ from app.forms.catalog.search_product_catalog_form import SearchProductCatalogFo
 
 
 
-class ProductCatalogView(View):
+class SearchProductView(View):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.product_service = ProductService()
 
-    def get(self, request):
-        try:
-          form = SearchProductCatalogForm()
-          products = self.product_service.get_all_products()
-
-          return render(request, "catalog/product_catalog.html", {
-              "products": products,
-              "form": form
-          })
-        except Exception as e:
-          print(e)
-
     def post(self, request):
         form = SearchProductCatalogForm(request.POST)
+
         if form.is_valid():
             products = self.product_service.search_products(form.cleaned_data)
 
