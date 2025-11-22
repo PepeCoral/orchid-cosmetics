d7075485f289effee94fa59003325c24236859ed
Juan Prieto <juaprifer@alum.us.es>
2025-11-22 12:48:17 +0100

    feat: :sparkles: seleccion de metodos de pago funcional

--- src/app/models/user.py
+++ src/app/models/user.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
 class RoleOptions(models.TextChoices):
     USER = 'User', 'User'
     ADMIN = 'Admin', 'Admin'
@@ -10,14 +9,18 @@ class PaymentMethodOptions(models.TextChoices):
     CASH_ON_DELIVERY = 'contrarembolso', 'Contrarembolso'
     PAYMENT_GATEWAY = 'pasarela', 'Pasarela de Pago'
 
+# Opción completamente vacía
+PAYMENT_METHOD_CHOICES = [('', '')] + list(PaymentMethodOptions.choices)
+
 class User(AbstractUser):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     address = models.CharField(max_length=200, null=True, default=None)
     pay_method = models.CharField(
         max_length=20, 
-        choices=PaymentMethodOptions.choices, 
-        default=PaymentMethodOptions.PAYMENT_GATEWAY
+        choices=PAYMENT_METHOD_CHOICES,
+        default='',
+        blank=True,
     )
     role = models.CharField(max_length=20, choices=RoleOptions.choices, default=RoleOptions.USER)
     
