b4ba5e8277571a945bc79e11fe5f4c4a1b4a1842
Pepe Coral <coritochico@gmail.com>
2025-11-23 20:49:04 +0100

    fix: :bug: aaaa

--- src/app/migrations/0028_remove_order_paymethod_remove_user_role.py
+++ src/app/migrations/0028_remove_order_paymethod_remove_user_role.py
@@ -11,10 +11,6 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RemoveField(
-            model_name='order',
-            name='payMethod',
-        ),
-        migrations.RemoveField(
             model_name='user',
             name='role',
         ),
