942761229a2f14cee99708b19e989782f685a492
Miguel Garcia Vizcaino <miguelgvizcaino@gmail.com>
2025-11-23 14:55:23 +0100

    feat: :zap: añadir restricciones

--- src/app/views/admin/order/shipped_status_update_view.py
+++ src/app/views/admin/order/shipped_status_update_view.py
@@ -13,11 +13,11 @@ class ShippedStatusUpdateView(View):
 
         if not request.user.is_superuser:
             return redirect("/")
-        
+
         orders = self.order_service.get_all_orders()
-        print("Llegue aqui")
+        
         try:
-            print(f"Updating order {order_id} to shipped")
+            
             self.order_service.update_order_status_to_shipped(order_id)
             return redirect(f"/admin/orders")
         except Exception as e:
